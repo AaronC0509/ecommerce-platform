013e9301d347dd1e164d413cdd81fb8fd8366033
Jia Zheng Aaron Chong <aaron.chong@azentio.com>
2022-06-07 16:30:50 +0800

    Add routing for the components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    return (<div className={`${size} menu-item`} onClick={ () => navigate(`${pathname}${linkUrl}`)}>
         <div className="background-image" 
             style={{
                 backgroundImage: `url(${imageUrl})`
@@ -12,7 +16,8 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <h1 className="title">{ title.toUpperCase() }</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
-    </div> 
-);
+    </div>
+    )
+};
 
 export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,6 +7,6 @@ const HomePage = () => (
     <div className="homepage">
         <Directory></Directory>
     </div>
-)
+);
 
 export default HomePage;
